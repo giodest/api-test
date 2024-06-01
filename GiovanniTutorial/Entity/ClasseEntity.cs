634d73e582f3670c9eb5cf4e074345df6007294e
Giovanni Destratis <destratisgiovanni2@gmail.com>
2024-06-01 20:32:28 +0200

    ClasseEntity creata

---
+++ GiovanniTutorial/Entity/ClasseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GiovanniTutorial.Entity
+{
+	public class ClasseEntity
+	{
+		public ClasseEntity()
+		{
+
+		}
+		public ClasseEntity(string sezione, string anno)
+		{
+			Id = Guid.NewGuid().ToString();
+			Sezione = sezione;
+			Anno = anno;
+		}
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public string? Id { get; set; }
+		public string Sezione { get; set; } = string.Empty;
+		public string Anno { get; set; } = string.Empty;
+	}
+}
--- GiovanniTutorial/obj/Debug/net8.0/GiovanniTutorial.AssemblyInfo.cs
+++ GiovanniTutorial/obj/Debug/net8.0/GiovanniTutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GiovanniTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e73de363737b08aa2168a4fdc4f3563376f3245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb94d7e90fa68421c7bdbc789f2043de3105655")]
 [assembly: System.Reflection.AssemblyProductAttribute("GiovanniTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GiovanniTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
