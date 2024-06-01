80b73987a4109a4134f3c5c5cf743cbbea4c849a
Giovanni Destratis <destratisgiovanni2@gmail.com>
2024-06-01 20:40:44 +0200

    Dipendency Injection ClasseService

--- GiovanniTutorial/Program.cs
+++ GiovanniTutorial/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IAlunnoService, AlunnoService>();
+builder.Services.AddScoped<IClasseService, ClasseService>();
 
 var app = builder.Build();
 
