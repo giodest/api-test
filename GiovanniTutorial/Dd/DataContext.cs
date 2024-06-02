a65cb5a4d723375a4db5b9d2123bee7cd8b1516f
Giovanni Destratis <destratisgiovanni2@gmail.com>
2024-06-03 00:08:29 +0200

    DataContext update

--- GiovanniTutorial/Dd/DataContext.cs
+++ GiovanniTutorial/Dd/DataContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GiovanniTutorial.Entity;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace GiovanniTutorial.Dd
 {
 	public class DataContext : DbContext 
 	{
-		public DataContext(DbContextOptions<DataContext> options) : base(options) {
-		
-		}
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<AlunnoEntity> Alunni {  get; set; }
+		public DbSet<ClasseEntity> Classe { get; set; }
 	}
 
 }
